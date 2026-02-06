aed9ef3bf2c1b028ddc9c489e0ab176eff85e458
Jean Sousa <jjeanssousa16@gmail.com>
2026-02-06 19:36:29 -0300

    produto camiseta id 2 criado.

--- Main.java
+++ Main.java
@@ -2,8 +2,10 @@ public class Main {
     public static void main(String[] args) {
         
         Produtos produto1 = new Produtos(1, "Boné", 19.99);
+        Produtos produto2 = new Produtos(2, "Camiseta", 58.60);
 
         produto1.exibirDados();
+        produto2.exibirDados();
 
     }
 }
